e0e8fd38daf533599ecfae7d6b19ad7e68d03c18
Kostov, Toni <toni.kostov@study.thws.de>
2025-11-13 14:25:15 +0200

    update useName.js to use async data storage

--- state-async-counter-app/store/useName.js
+++ state-async-counter-app/store/useName.js
@@ -1,9 +1,15 @@
 import {create} from 'zustand'
+import {persist, createJSONStorage} from 'zustand/middleware';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-
-const useName = create((set) => ({
-    name: '',
-    setName: (enteredName) => set(() => ({name: enteredName})),
-}))
+const useName = create(
+    persist((set) => ({
+            name: '',
+            setName: (enteredName) => set(() => ({name: enteredName})),
+        }),
+        {
+            name: 'name-storage',
+            storage: createJSONStorage(() => AsyncStorage)
+        }));
 
 export default useName;
