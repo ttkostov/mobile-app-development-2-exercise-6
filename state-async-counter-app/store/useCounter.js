49c95a70ddcd55c89b876814529713f48834d2eb
Kostov, Toni <toni.kostov@study.thws.de>
2025-11-13 14:15:45 +0200

    update useCounter.js to use async storage

--- state-async-counter-app/store/useCounter.js
+++ state-async-counter-app/store/useCounter.js
@@ -1,11 +1,20 @@
-import {create} from 'zustand'
+import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-
- const useCounter = create((set) => ({
-    count: 0,
-    incrementCounter: () => set((state) => ({count: state.count + 1})),
-    decrementCounter: () => set((state) => ({count: state.count - 1})),
-    resetCounter: () => set(() => ({count: 0})),
-}))
+const useCounter = create(
+    persist(
+        (set) => ({
+            count: 0,
+            incrementCounter: () => set((state) => ({ count: state.count + 1 })),
+            decrementCounter: () => set((state) => ({ count: state.count - 1 })),
+            resetCounter: () => set({ count: 0 }),
+        }),
+        {
+            name: 'counter-storage',
+            storage: createJSONStorage(() => AsyncStorage),
+        }
+    )
+);
 
 export default useCounter;
