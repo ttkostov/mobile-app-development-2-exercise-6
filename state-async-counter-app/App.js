b32105cab7bf2bf7a58b66ada7d2b0e553604d61
Kostov, Toni <toni.kostov@study.thws.de>
2025-11-13 14:15:31 +0200

    use native bottom tab navigator

--- state-async-counter-app/App.js
+++ state-async-counter-app/App.js
@@ -1,47 +1,42 @@
 import {StatusBar} from 'expo-status-bar';
 import {Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native'
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createNativeBottomTabNavigator} from '@react-navigation/bottom-tabs/unstable';
 import CounterScreen from "./screens/CounterScreen";
 import DisplayScreen from "./screens/DisplayScreen";
 import {Provider as PaperProvider} from 'react-native-paper';
 
 
-const Tab = createBottomTabNavigator();
+const Tab = createNativeBottomTabNavigator();
 
 export default function App() {
-  return (
-      <PaperProvider>
-        <NavigationContainer>
-          {
-            <Tab.Navigator id='main-tab-navigator'
-                           screenOptions={{
-                             headerShown: false,
-                             tabBarIconStyle: {marginBottom: 7},
-                             tabBarLabelStyle: {fontSize: 15},
-                           }}
-            >
-              <Tab.Screen name="Counter"
-                          component={CounterScreen}
-                          options={{
-                            tabBarIcon: ({color, size}) => (
-                                <Text style={{color: color, fontSize: size}}>🔢️</Text>
-                            )
-                          }}/>
-              <Tab.Screen
-                  name="Display"
-                  component={DisplayScreen}
-                  options={{
-                    tabBarIcon: ({color, size}) => (
-                        <Text style={{color: color, fontSize: size}}>🖥️</Text>
-                    )
-                  }}
-              />
-            </Tab.Navigator>
-          }
-        </NavigationContainer>
-        <StatusBar/>
+    return (
+        <PaperProvider>
+            <StatusBar/>
+            <NavigationContainer>
 
-      </PaperProvider>
-  );
+                <Tab.Navigator>
+                    <Tab.Screen name="Counter"
+                                component={CounterScreen}
+                                options={{
+
+                                    tabBarIcon: {
+                                        type: "drawableResource",
+                                        name: "heart_icon"
+                                    }
+                                }}/>
+                    <Tab.Screen
+                        name="Display"
+                        component={DisplayScreen}
+                        options={{
+                            tabBarIcon: ({color, size}) => (
+                                <Text style={{color: color, fontSize: size}}>🖥️</Text>
+                            )
+                        }}
+                    />
+                </Tab.Navigator>
+
+            </NavigationContainer>
+        </PaperProvider>
+    );
 }
